1afe2d1a5a49d097e1fe0037e77797a2b2a26822
John Michael <2014068886@ust-ics.mygbiz.com>
2018-01-05 23:43:12 +0800

    First Commit

---
+++ app/src/main/java/com/ics115_4ite_bayocot_lina_sideco_zalameda/leadsv2/AppConfig.java
@@ -0,0 +1,15 @@
+package com.ics115_4ite_bayocot_lina_sideco_zalameda.leadsv2;
+
+/**
+ * Created by JMB on 1/5/2018.
+ */
+
+public class AppConfig {
+
+        // Server user login url
+        public static String URL_LOGIN = "http://112.208.32.299/android_login_api/login.php";
+
+        // Server user register url
+        public static String URL_REGISTER = "http://112.208.32.299/android_login_api/register.php";
+
+}
---
+++ app/src/main/java/com/ics115_4ite_bayocot_lina_sideco_zalameda/leadsv2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ics115_4ite_bayocot_lina_sideco_zalameda.leadsv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
